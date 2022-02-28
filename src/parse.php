92692ffb3f5a479e225f5091158df0e29a69f7a9
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2022-02-28 22:06:38 +0100

    Add class autoloading

--- src/parse.php
+++ src/parse.php
@@ -1,7 +1,21 @@
 <?php
+/**
+ * This is a part of IPP project
+ *
+ * @author Michal Šmahel (xsmahe01)
+ * @date 2022
+ */
 
 declare(strict_types=1);
 
 ini_set('display_errors', 'stderr');
+
+spl_autoload_register(function(string $fullyQualifiedClassName) {
+    $withoutPrefix = str_replace('App\\', '', $fullyQualifiedClassName);
+    $asPath = str_replace('\\', '/', $withoutPrefix);
+
+    /** @noinspection PhpIncludeInspection Generated path */
+    require_once __DIR__ . "/php/{$asPath}.php";
+});
 
 echo 'Hello world!';
