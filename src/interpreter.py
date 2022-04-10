74cd9401fd681a315b6777c70f3d9d886c5d48e7
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2022-04-10 22:41:07 +0200

    Add basics of interpreter part

---
+++ src/interpreter.py
@@ -0,0 +1,12 @@
+# This is a part of IPP project
+#
+# Author: Michal Šmahel (xsmahe01)
+# Date: 2021
+
+def main():
+    """Main function controlling the running of the script"""
+    pass
+
+
+if __name__ == '__main__':
+    main()
